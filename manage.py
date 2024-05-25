4804a9219b4997f4b88ee964360832e888482000
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-05-25 18:33:32 +0200

    Django Configurations

--- manage.py
+++ manage.py
@@ -7,10 +7,12 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
     #os.environ.setdefault("DJANGO_CONFIGURATION", "prod")
+    
     try:
-        from django.core.management import execute_from_command_line
-        #from configurations.management import execute_from_command_line
+        #from django.core.management import execute_from_command_line
+        from configurations.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
--- project/urls.py
+++ project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+print(f"Time zone: {settings.TIME_ZONE}", "\n")
+
 # other imports
 import blog.views
 
